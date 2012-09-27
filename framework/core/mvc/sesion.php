d35aba7f8990b9e3fcf3137aab53759c1c0ca5d6
ebmoya <ebuenadicha@todovino.com>
2012-09-27 15:51:33 +0200

    5

--- framework/core/mvc/sesion.php
+++ framework/core/mvc/sesion.php
@@ -5,10 +5,10 @@ $urltotal=$_SERVER['REQUEST_URI'];
 
 $sessD=explode("\?SSID=",$urltotal);
 
-$vars[url]=$sessD[0];
-$vars[SSID]=$sessD[1];
+$vars['url']=$sessD[0];
+$vars['SSID']=$sessD[1];
 
 ### compruebo admintemporal
-if($vars[SSID]=="vero"){$vars[MODadmin]=true;}else{$vars[MODadmin]=FALSE;};
+if($vars['SSID']=="vero"){$vars['MODadmin']=true;}else{$vars['MODadmin']=FALSE;};
 
 ?>
