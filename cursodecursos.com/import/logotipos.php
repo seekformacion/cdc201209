cd367e6b30832309971b155f28a2f516a9fee70b
ebmoya <ebuenadicha@todovino.com>
2012-09-27 17:16:44 +0200

    dd

--- cursodecursos.com/import/logotipos.php
+++ cursodecursos.com/import/logotipos.php
@@ -4,7 +4,7 @@ set_time_limit(0);
 
 include "../scripts/variables.php";
 
-
+echo exif_imagetype('http://procenet:nuevaof21@82.223.155.233:81/logos.php?tipo=g&idcentro=939');
 
 $content2 = file_get_contents("http://procenet:nuevaof21@82.223.155.233:81/logos.php?tipo=g&idcentro=939");
 $fp = fopen($paths[www] . "/logos/masterd.gif", "w");
